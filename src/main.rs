41ecb6e7e141cb8dea0c54e1d7b2b1fe09f3d671
Alexander Serebryakov <alex.serebr@gmail.com>
2024-07-24 15:42:06 +0300

    Update to latest frankenstein

--- src/main.rs
+++ src/main.rs
@@ -13,9 +13,12 @@ static BASE_API_URL: &str = "https://api.telegram.org/bot";
 async fn main() {
     let token = env::var("TELEGRAM_BOT_TOKEN").expect("TELEGRAM_BOT_TOKEN not set");
     let api_url = format!("{}{}", BASE_API_URL, token);
-    let api = AsyncApi::new_with_client(Client::builder().danger_accept_invalid_certs(true).build().unwrap(), api_url); 
+    // let api = AsyncApi::new_with_client(Client::builder().danger_accept_invalid_certs(true).build().unwrap(), api_url);
     let update_params_builder = GetUpdatesParams::builder();
     let mut update_params = update_params_builder.clone().build();
+
+    let client= Client::builder().danger_accept_invalid_certs(true).build().unwrap();
+    let api = AsyncApi::builder().api_url(api_url).client(client).build();
 
     loop {
         let result = api.get_updates(&update_params).await;
@@ -49,10 +52,7 @@ async fn main() {
 async fn process_message(message: Message, api: AsyncApi) {
     let send_message_params = SendMessageParams::builder()
         .chat_id(message.chat.id)
-        //.text(format!("hello {}, you wrote \'{}\'", message.from.unwrap().username.unwrap(), message.text.unwrap()))
-        .text(format!("hello, you wrote \'{}\'", message.text.unwrap()))
-        .reply_to_message_id(message.message_id)
-        .build();
+        .text(format!("hello, you wrote \'{}\'", message.text.unwrap())).build();
 
     if let Err(err) = api.send_message(&send_message_params).await {
         println!("Failed to send message: {:?}", err);
